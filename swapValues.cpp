a53d3bc2b2bdb9c16f89ae8f01d4b864f7218a98
CelloSki <Selorm13ahadjie@gmail.com>
2025-02-26 16:53:01 +0000

    Added swapping of two values without a third variable.

---
+++ swapValues.cpp
@@ -0,0 +1,43 @@
+//
+// Created by CelloSki on 2/26/2025.
+//
+//****************************************************************************
+// Program Functionality and Purpose
+// Swapping of two values
+//****************************************************************************
+
+// header file
+#include <iostream>
+
+using namespace std;
+
+int main() {
+// Declare variables
+int a, b;
+
+//prompt user to enter two values.
+cout << "Please enter the Value for A: ";
+cin >> a;
+cin.clear();
+cout << endl;
+
+cout << "Please enter the Value for B: ";
+cin >> b;
+cin.clear();
+cout << endl;
+
+//Before swap
+cout << "The values for A: " << a << " and B: " << b  << " before swap." << endl;
+
+
+//swapping values
+a = a + b;
+b = a - b;
+a = a -b;
+
+//After swapping
+cout << "The values for A: " << a << " and B: " << b << " after swap." << endl;
+
+
+return 0;
+}
